90c658e49d195833fe3dd29559ddacf9bcaea55f
Christian Pschorr <pschorr.christian@gmail.com>
2019-07-20 15:04:36 +0200

    0.0.1@2019-07-20

--- app/Traits/Properties.php
+++ app/Traits/Properties.php
@@ -5,6 +5,11 @@ namespace App\Traits;
 use Illuminate\Support\Str;
 
 trait Properties {
+
+	/**
+	 * @param array $properties
+	 * @return void
+	 */
 	public function fill($properties) {
 		foreach($properties as $key => $value) {
 			$method = 'set' . Str::studly($key);
